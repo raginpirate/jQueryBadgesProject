d6c381ba0b06e43911b05d6cb0bdec638acaeec0
unknown <dcwyckof@git.uwaterloo.ca>
2018-02-04 07:09:11 -0500

    uploaded file

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,18 @@
-$(function() {
-
-  // your code will go here
-
-});
+$.ajax({
+    url: 'https://www.codeschool.com/users/najibk.json',
+    dataType: 'jsonp',
+    success: function(response) {
+		//# = tags, . = classes
+		for (x=0; x<response.courses.completed.length; x++) {
+			//my quotations might be a lil fuckd
+			var newDiv = $('<div class="course"></div>');
+			var newH = $('<h3>'+ response.courses.completed[x].title +'</h3>');
+			var newImg = $('<img src="' + response.courses.completed[x].badge + '"></img>');
+			var newA = $('<a href="' + response.courses.completed[x].url + '" target="_blank" class="btn btn-primary">See Course</a>');
+			$('#badges').append(newDiv);
+			$('#badges').children('.course').last().append(newH);
+			$('#badges').children('.course').last().append(newImg);
+			$('#badges').children('.course').last().append(newA);
+		}
+    }
+  });
